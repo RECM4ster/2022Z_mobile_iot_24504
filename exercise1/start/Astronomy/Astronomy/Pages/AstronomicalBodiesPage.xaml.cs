bac65210ebbc9f7761ef0d21209649a186d8d8e3
M4sterrr <grzegorz091499@gmail.com>
2023-01-09 21:22:50 +0100

    add lab 5

---
+++ exercise1/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Astronomy.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AstronomicalBodiesPage : ContentPage
+	{
+        public AstronomicalBodiesPage()
+        {
+            InitializeComponent();
+
+            btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+            btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+            btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+            btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+        }
+    }
+}
